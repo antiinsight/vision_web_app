9af4253ba33ea95b010244296b6e2b8133c21c6e
canthonyscott <anthony@canthonyscott.com>
2016-03-05 11:05:17 -0600

    allows responses to be calculated from 48 well plates
    Have new if statements that can calculate 48 and 96 well plate
    differences. Response table for 48 well plate does not work yet

--- server.R
+++ server.R
@@ -27,6 +27,7 @@ shinyServer(function(input, output) {
     inData <- input$data
     threshold <- input$threshold
     inSample <- input$sample
+    inPlateNum <- input$plateSelect
     
     # creates a progress bar to show the user its doing something in the background
     withProgress(message = "Rendering Plot... Please Wait", value = 0, {
@@ -96,15 +97,13 @@ shinyServer(function(input, output) {
       threshold <- input$threshold
       inGroup <- input$group
       inSample <- input$sample
+      inPlateNum <- input$plateSelect
       
       # make sure uploaded file is not NULL, prevents errors, only if using user data
       if (inSample == FALSE){
         if (is.null(inFile))
           return(NULL)
       }
-
-   withProgress(message = "Rendering... Please Wait", value = 0, {   
-      
 
       
      # read in raw data and append well numbers, doing this here instead of in visioncounts_perwell function theoretically decreases run-time
@@ -120,7 +119,11 @@ shinyServer(function(input, output) {
       # the recording stop time 
       rawdata <- subset(rawdata, rawdata$end <= 1950)
       
-      rawdata$wellnum <- 1:96
+      if (inPlateNum == 96){
+        rawdata$wellnum <- 1:96
+      } else if (inPlateNum == 48 ){
+        rawdata$wellnum <- 1:48
+      }
  
       # needed variables for time, originally were input by user, but hard-coding these creates a cleaner UI
       time1 <- 1800
@@ -129,63 +132,63 @@ shinyServer(function(input, output) {
       time4 <- 1890
       time5 <- 1920
       
-      ## render plots for individual animals sorted by row, only if normalization is selected
-
-
-          # make a loop to cycle through all 96 wells and assign it a variable based on Row (example: well 1 = A1, well 96 = H12)       
-          for (i in 1:12)
-          {
-            
-            assign(paste("A",i, sep=""), visioncounts_perwell(i, rawdata, time1, time2, time3, time4, time5, threshold))
-            assign(paste("B",i, sep=""), visioncounts_perwell(i+12, rawdata, time1, time2, time3, time4, time5, threshold))
-            assign(paste("C",i, sep=""), visioncounts_perwell(i+24, rawdata, time1, time2, time3, time4, time5, threshold))
-            assign(paste("D",i, sep=""), visioncounts_perwell(i+36, rawdata, time1, time2, time3, time4, time5, threshold))
-            assign(paste("E",i, sep=""), visioncounts_perwell(i+48, rawdata, time1, time2, time3, time4, time5, threshold))
-            assign(paste("F",i, sep=""), visioncounts_perwell(i+60, rawdata, time1, time2, time3, time4, time5, threshold))
-            assign(paste("G",i, sep=""), visioncounts_perwell(i+72, rawdata, time1, time2, time3, time4, time5, threshold))
-            assign(paste("H",i, sep=""), visioncounts_perwell(i+84, rawdata, time1, time2, time3, time4, time5, threshold))
-            incProgress(amount = i/12)
-          }
+      if (inPlateNum == 96){
+           withProgress(message = "Rendering... Please Wait", value = 0, {   
+        
+        # make a loop to cycle through all 96 wells and assign it a variable based on Row (example: well 1 = A1, well 96 = H12)       
+        for (i in 1:12)
+        {
           
-          #create vector array by row
-          A <- c(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12)
-          B <- c(B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12)
-          C <- c(C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12)
-          D <- c(D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12)
-          E <- c(E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12)
-          F <- c(F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12)
-          G <- c(G1, G2, G3, G4, G5, G6, G7, G8, G9, G10, G11, G12)
-          H <- c(H1, H2, H3, H4, H5, H6, H7, H8, H9, H10, H11, H12)
+          assign(paste("A",i, sep=""), visioncounts_perwell(i, rawdata, time1, time2, time3, time4, time5, threshold))
+          assign(paste("B",i, sep=""), visioncounts_perwell(i+12, rawdata, time1, time2, time3, time4, time5, threshold))
+          assign(paste("C",i, sep=""), visioncounts_perwell(i+24, rawdata, time1, time2, time3, time4, time5, threshold))
+          assign(paste("D",i, sep=""), visioncounts_perwell(i+36, rawdata, time1, time2, time3, time4, time5, threshold))
+          assign(paste("E",i, sep=""), visioncounts_perwell(i+48, rawdata, time1, time2, time3, time4, time5, threshold))
+          assign(paste("F",i, sep=""), visioncounts_perwell(i+60, rawdata, time1, time2, time3, time4, time5, threshold))
+          assign(paste("G",i, sep=""), visioncounts_perwell(i+72, rawdata, time1, time2, time3, time4, time5, threshold))
+          assign(paste("H",i, sep=""), visioncounts_perwell(i+84, rawdata, time1, time2, time3, time4, time5, threshold))
+          incProgress(amount = i/12)
+        }
+        
+        #create vector array by row
+        A <- c(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12)
+        B <- c(B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12)
+        C <- c(C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12)
+        D <- c(D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12)
+        E <- c(E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11, E12)
+        F <- c(F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12)
+        G <- c(G1, G2, G3, G4, G5, G6, G7, G8, G9, G10, G11, G12)
+        H <- c(H1, H2, H3, H4, H5, H6, H7, H8, H9, H10, H11, H12)
+        
+        
+        # average resposnes from each Row of individual fish
+        A.m <- mean(A)
+        B.m <- mean(B)
+        C.m <- mean(C)
+        D.m <- mean(D)
+        E.m <- mean(E)
+        F.m <- mean(F)
+        G.m <- mean(G)
+        H.m <- mean(H)
+        
+        # standard error of each row          
+        A.se <- StdErr(A, length(A))
+        B.se <- StdErr(B, length(B))
+        C.se <- StdErr(C, length(C))
+        D.se <- StdErr(D, length(D))
+        E.se <- StdErr(E, length(E))
+        F.se <- StdErr(F, length(F))
+        G.se <- StdErr(G, length(G))
+        H.se <- StdErr(H, length(H))
+              })    
+        
+        # plot every row as an individual bar if user hasnt grouped rows
+        if (inGroup == FALSE)
+        {
           
+          Means <- c(A.m, B.m, C.m, D.m, E.m, F.m, G.m, H.m)
+          stErrs <- c(A.se, B.se, C.se, D.se, E.se, F.se, G.se, H.se)
           
-          # average resposnes from each Row of individual fish
-          A.m <- mean(A)
-          B.m <- mean(B)
-          C.m <- mean(C)
-          D.m <- mean(D)
-          E.m <- mean(E)
-          F.m <- mean(F)
-          G.m <- mean(G)
-          H.m <- mean(H)
-                  
-          # standard error of each row          
-          A.se <- StdErr(A, length(A))
-          B.se <- StdErr(B, length(B))
-          C.se <- StdErr(C, length(C))
-          D.se <- StdErr(D, length(D))
-          E.se <- StdErr(E, length(E))
-          F.se <- StdErr(F, length(F))
-          G.se <- StdErr(G, length(G))
-          H.se <- StdErr(H, length(H))
-      })    
-          
-          # plot every row as an individual bar if user hasnt grouped rows
-          if (inGroup == FALSE)
-          {
-            
-            Means <- c(A.m, B.m, C.m, D.m, E.m, F.m, G.m, H.m)
-            stErrs <- c(A.se, B.se, C.se, D.se, E.se, F.se, G.se, H.se)
-            
           # bar plot the averages from individual rows   
           mp <- barplot(c(A.m, B.m, C.m, D.m, E.m, F.m, G.m, H.m), axes=FALSE, axisnames=FALSE, ylim=c(0,5), main="Average Responses Per Row")
           axis(1, labels=c("A", "B","C","D", "E","F","G","H"), at = mp)
@@ -197,9 +200,73 @@ shinyServer(function(input, output) {
           segments(mp - 0.1, Means - stErrs, mp + 0.1, Means - stErrs, lwd=2)
           segments(mp - 0.1, Means + stErrs, mp + 0.1, Means + stErrs, lwd=2)
           
+        }
+        
+      }
+      # calling functions for only using 48 well plate, repeat of above with less column and rows
+      else if (inPlateNum == 48){
+        withProgress(message = "Rendering... Please Wait", value = 0, {   
+          
+          # make a loop to cycle through all 96 wells and assign it a variable based on Row (example: well 1 = A1, well 96 = H12)       
+          for (i in 1:8)
+          {
+            
+            assign(paste("A",i, sep=""), visioncounts_perwell(i, rawdata, time1, time2, time3, time4, time5, threshold))
+            assign(paste("B",i, sep=""), visioncounts_perwell(i+8, rawdata, time1, time2, time3, time4, time5, threshold))
+            assign(paste("C",i, sep=""), visioncounts_perwell(i+16, rawdata, time1, time2, time3, time4, time5, threshold))
+            assign(paste("D",i, sep=""), visioncounts_perwell(i+24, rawdata, time1, time2, time3, time4, time5, threshold))
+            assign(paste("E",i, sep=""), visioncounts_perwell(i+32, rawdata, time1, time2, time3, time4, time5, threshold))
+            assign(paste("F",i, sep=""), visioncounts_perwell(i+40, rawdata, time1, time2, time3, time4, time5, threshold))
+            incProgress(amount = i/8)
           }
           
+          #create vector array by row
+          A <- c(A1, A2, A3, A4, A5, A6, A7, A8)
+          B <- c(B1, B2, B3, B4, B5, B6, B7, B8)
+          C <- c(C1, C2, C3, C4, C5, C6, C7, C8)
+          D <- c(D1, D2, D3, D4, D5, D6, D7, D8)
+          E <- c(E1, E2, E3, E4, E5, E6, E7, E8)
+          F <- c(F1, F2, F3, F4, F5, F6, F7, F8)
+
+          
+          # average resposnes from each Row of individual fish
+          A.m <- mean(A)
+          B.m <- mean(B)
+          C.m <- mean(C)
+          D.m <- mean(D)
+          E.m <- mean(E)
+          F.m <- mean(F)
+
+          # standard error of each row          
+          A.se <- StdErr(A, length(A))
+          B.se <- StdErr(B, length(B))
+          C.se <- StdErr(C, length(C))
+          D.se <- StdErr(D, length(D))
+          E.se <- StdErr(E, length(E))
+          F.se <- StdErr(F, length(F))
+        })    
         
+        # plot every row as an individual bar if user hasnt grouped rows
+        if (inGroup == FALSE)
+        {
+          
+          Means <- c(A.m, B.m, C.m, D.m, E.m, F.m)
+          stErrs <- c(A.se, B.se, C.se, D.se, E.se, F.se)
+          
+          # bar plot the averages from individual rows   
+          mp <- barplot(c(A.m, B.m, C.m, D.m, E.m, F.m), axes=FALSE, axisnames=FALSE, ylim=c(0,5), main="Average Responses Per Row")
+          axis(1, labels=c("A", "B","C","D", "E","F"), at = mp)
+          axis(2, at=seq(0,5, by=1))
+          box()
+          Means <- c(A.m, B.m, C.m, D.m, E.m, F.m)
+          stErrs <- matrix(stErrs <- c(A.se, B.se, C.se, D.se, E.se, F.se),6)
+          segments(mp, Means - stErrs, mp, Means + stErrs, lwd=2)
+          segments(mp - 0.1, Means - stErrs, mp + 0.1, Means - stErrs, lwd=2)
+          segments(mp - 0.1, Means + stErrs, mp + 0.1, Means + stErrs, lwd=2)
+          
+        }
+        
+      }
           
           # Combine bars and plot based on users input
           if (inGroup == TRUE)
@@ -360,6 +427,7 @@ shinyServer(function(input, output) {
       threshold <- input$threshold
       individual <- input$individual
       inSample <- input$sample
+      inPlateNum <- input$plateSelect
       
       # make sure uploaded file is not NULL; prevents errors, but only if Sample Data is not being used
       if (inSample == FALSE){
@@ -395,8 +463,13 @@ shinyServer(function(input, output) {
       # create variables for counts of every well
       # TODO: Create the loop repetition as a function where you just input the row letter
       
+      if (inPlateNum == 96){
+        col_num <- 12
+      } else if (inPlateNum == 48){
+        col_num <- 8
+      }
       
-      for (i in 1:12)
+      for (i in 1:col_num)
       {
         resp <- visioncounts_perwell(i, rawdata, time1, time2, time3, time4, time5, threshold)
         if (is.null(resp)){
@@ -407,7 +480,7 @@ shinyServer(function(input, output) {
       rowA <- c(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12)
       incProgress(amount = 1/8)
       
-      for (i in 1:12)
+      for (i in 1:col_num)
       {
         resp <- visioncounts_perwell(i+12, rawdata, time1, time2, time3, time4, time5, threshold)
         if (is.null(resp)){
@@ -418,7 +491,7 @@ shinyServer(function(input, output) {
       rowB <- c(B1,B2,B3,B4,B5,B6,B7,B8,B9,B10,B11,B12)
       incProgress(amount = 2/8)
       
-      for (i in 1:12)
+      for (i in 1:col_num)
       {
         resp <- visioncounts_perwell(i+24, rawdata, time1, time2, time3, time4, time5, threshold)
         if (is.null(resp)){
@@ -430,7 +503,7 @@ shinyServer(function(input, output) {
       incProgress(amount = 3/8)
       
       
-      for (i in 1:12)
+      for (i in 1:col_num)
       {
         resp <- visioncounts_perwell(i+36, rawdata, time1, time2, time3, time4, time5, threshold)
         if (is.null(resp)){
@@ -442,7 +515,7 @@ shinyServer(function(input, output) {
       incProgress(amount = 4/8)
       
       
-      for (i in 1:12)
+      for (i in 1:col_num)
       {
         resp <- visioncounts_perwell(i+48, rawdata, time1, time2, time3, time4, time5, threshold)
         if (is.null(resp)){
@@ -454,7 +527,7 @@ shinyServer(function(input, output) {
       incProgress(amount = 5/8)
       
       
-      for (i in 1:12)
+      for (i in 1:col_num)
       {
         resp <- visioncounts_perwell(i+60, rawdata, time1, time2, time3, time4, time5, threshold)
         if (is.null(resp)){
@@ -465,34 +538,34 @@ shinyServer(function(input, output) {
       rowF <- c(F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12)
       incProgress(amount = 6/8)
       
-      
-      for (i in 1:12)
-      {
-        resp <- visioncounts_perwell(i+72, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
+        for (i in 1:col_num)
+        {
+          resp <- visioncounts_perwell(i+72, rawdata, time1, time2, time3, time4, time5, threshold)
+          if (is.null(resp)){
+            resp <- "NA"
+          }
+          assign(paste("G",i, sep=""),resp)
         }
-        assign(paste("G",i, sep=""),resp)
-      }
-      rowG <- c(G1,G2,G3,G4,G5,G6,G7,G8,G9,G10,G11,G12)
-      incProgress(amount = 7/8)
-      
-      
-      for (i in 1:12)
-      {
-        resp <- visioncounts_perwell(i+84, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
+        rowG <- c(G1,G2,G3,G4,G5,G6,G7,G8,G9,G10,G11,G12)
+        incProgress(amount = 7/8)
+        
+        
+        for (i in 1:col_num)
+        {
+          resp <- visioncounts_perwell(i+84, rawdata, time1, time2, time3, time4, time5, threshold)
+          if (is.null(resp)){
+            resp <- "NA"
+          }
+          assign(paste("H",i, sep=""), resp)
         }
-        assign(paste("H",i, sep=""), resp)
-      }
-      rowH <- c(H1,H2,H3,H4,H5,H6,H7,H8,H9,H10,H11,H12)
-      incProgress(amount = 1)
+        rowH <- c(H1,H2,H3,H4,H5,H6,H7,H8,H9,H10,H11,H12)
+        incProgress(amount = 1)
+        
+        
+        wells <- c("Well 1", "Well 2", "Well 3", "Well 4", "Well 5", "Well 6", "Well 7", "Well 8", "Well 9", "Well 10", "Well 11", "Well 12")
+        df <- data.frame(wells, rowA,rowB,rowC,rowD,rowE,rowF,rowG,rowH)
       
       
-      wells <- c("Well 1", "Well 2", "Well 3", "Well 4", "Well 5", "Well 6", "Well 7", "Well 8", "Well 9", "Well 10", "Well 11", "Well 12")
-      df <- data.frame(wells, rowA,rowB,rowC,rowD,rowE,rowF,rowG,rowH)
-
       })
       
       ## Allow downloading of CSV file of df table, this must be within the scope of the generated df
@@ -505,17 +578,17 @@ shinyServer(function(input, output) {
       
       ## display the df on the page, needed after download handler
       return (df)
-    
+      
     })
     
     
-  # renders the changelog HTML file as a reactive variable to be displayed in the changelog tab in the UI
-  # allows the changelog to be kept in a file outside of the R scripts
-  output$changelog <- renderUI({
-    HTML(readLines("www/changelog.html", warn = FALSE))
+    # renders the changelog HTML file as a reactive variable to be displayed in the changelog tab in the UI
+    # allows the changelog to be kept in a file outside of the R scripts
+    output$changelog <- renderUI({
+      HTML(readLines("www/changelog.html", warn = FALSE))
+      
+    })
     
-  })
-  
   # render the intstructions tab from an html file
   #allows the intro to be kept in a file outside of the R scripts
   output$intro <- renderUI({
