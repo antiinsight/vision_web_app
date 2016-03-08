1c409174dccf4e0ed80c9be134aad49f405fb370
canthonyscott <anthony@canthonyscott.com>
2016-03-07 20:44:54 -0600

    48 well plate fully functional
    The 48 well plate now functions completely. Added final code needed to
    correctly calculate the individual response spreadsheet.

--- server.R
+++ server.R
@@ -463,109 +463,181 @@ shinyServer(function(input, output) {
       # create variables for counts of every well
       # TODO: Create the loop repetition as a function where you just input the row letter
       
-      if (inPlateNum == 96){
-        col_num <- 12
-      } else if (inPlateNum == 48){
-        col_num <- 8
+  if (inPlateNum == 96){
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
       }
-      
-      for (i in 1:col_num)
-      {
-        resp <- visioncounts_perwell(i, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
-        }
-        assign(paste("A",i, sep=""), resp)
+      assign(paste("A",i, sep=""), resp)
+    }
+    rowA <- c(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12)
+    incProgress(amount = 1/8)
+    
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i+12, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
       }
-      rowA <- c(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12)
-      incProgress(amount = 1/8)
-      
-      for (i in 1:col_num)
-      {
-        resp <- visioncounts_perwell(i+12, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
-        }
-        assign(paste("B",i, sep=""), resp)
+      assign(paste("B",i, sep=""), resp)
+    }
+    rowB <- c(B1,B2,B3,B4,B5,B6,B7,B8,B9,B10,B11,B12)
+    incProgress(amount = 2/8)
+    
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i+24, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
       }
-      rowB <- c(B1,B2,B3,B4,B5,B6,B7,B8,B9,B10,B11,B12)
-      incProgress(amount = 2/8)
-      
-      for (i in 1:col_num)
-      {
-        resp <- visioncounts_perwell(i+24, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
-        }
-        assign(paste("C",i, sep=""), resp)
+      assign(paste("C",i, sep=""), resp)
+    }
+    rowC <- c(C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12)
+    incProgress(amount = 3/8)
+    
+    
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i+36, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
       }
-      rowC <- c(C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12)
-      incProgress(amount = 3/8)
-      
-      
-      for (i in 1:col_num)
-      {
-        resp <- visioncounts_perwell(i+36, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
-        }
-        assign(paste("D",i, sep=""), resp)
+      assign(paste("D",i, sep=""), resp)
+    }
+    rowD <- c(D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11,D12)
+    incProgress(amount = 4/8)
+    
+    
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i+48, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
       }
-      rowD <- c(D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11,D12)
-      incProgress(amount = 4/8)
-      
-      
-      for (i in 1:col_num)
-      {
-        resp <- visioncounts_perwell(i+48, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
-        }
-        assign(paste("E",i, sep=""), resp)
+      assign(paste("E",i, sep=""), resp)
+    }
+    rowE <- c(E1,E2,E3,E4,E5,E6,E7,E8,E9,E10,E11,E12)
+    incProgress(amount = 5/8)
+    
+    
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i+60, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
       }
-      rowE <- c(E1,E2,E3,E4,E5,E6,E7,E8,E9,E10,E11,E12)
-      incProgress(amount = 5/8)
-      
-      
-      for (i in 1:col_num)
-      {
-        resp <- visioncounts_perwell(i+60, rawdata, time1, time2, time3, time4, time5, threshold)
-        if (is.null(resp)){
-          resp <- "NA"
-        }
-        assign(paste("F",i, sep=""), resp)
+      assign(paste("F",i, sep=""), resp)
+    }
+    rowF <- c(F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12)
+    incProgress(amount = 6/8)
+    
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i+72, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
       }
-      rowF <- c(F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12)
-      incProgress(amount = 6/8)
-      
-        for (i in 1:col_num)
-        {
-          resp <- visioncounts_perwell(i+72, rawdata, time1, time2, time3, time4, time5, threshold)
-          if (is.null(resp)){
-            resp <- "NA"
-          }
-          assign(paste("G",i, sep=""),resp)
-        }
-        rowG <- c(G1,G2,G3,G4,G5,G6,G7,G8,G9,G10,G11,G12)
-        incProgress(amount = 7/8)
-        
-        
-        for (i in 1:col_num)
-        {
-          resp <- visioncounts_perwell(i+84, rawdata, time1, time2, time3, time4, time5, threshold)
-          if (is.null(resp)){
-            resp <- "NA"
-          }
-          assign(paste("H",i, sep=""), resp)
-        }
-        rowH <- c(H1,H2,H3,H4,H5,H6,H7,H8,H9,H10,H11,H12)
-        incProgress(amount = 1)
-        
-        
-        wells <- c("Well 1", "Well 2", "Well 3", "Well 4", "Well 5", "Well 6", "Well 7", "Well 8", "Well 9", "Well 10", "Well 11", "Well 12")
-        df <- data.frame(wells, rowA,rowB,rowC,rowD,rowE,rowF,rowG,rowH)
-      
-      
+      assign(paste("G",i, sep=""),resp)
+    }
+    rowG <- c(G1,G2,G3,G4,G5,G6,G7,G8,G9,G10,G11,G12)
+    incProgress(amount = 7/8)
+    
+    
+    for (i in 1:12)
+    {
+      resp <- visioncounts_perwell(i+84, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
+      }
+      assign(paste("H",i, sep=""), resp)
+    }
+    rowH <- c(H1,H2,H3,H4,H5,H6,H7,H8,H9,H10,H11,H12)
+    incProgress(amount = 1)
+    
+    
+    wells <- c("Well 1", "Well 2", "Well 3", "Well 4", "Well 5", "Well 6", "Well 7", "Well 8", "Well 9", "Well 10", "Well 11", "Well 12")
+    df <- data.frame(wells, rowA,rowB,rowC,rowD,rowE,rowF,rowG,rowH)
+    
+  } else if (inPlateNum == 48){
+    for (i in 1:8)
+    {
+      resp <- visioncounts_perwell(i, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
+      }
+      assign(paste("A",i, sep=""), resp)
+    }
+    rowA <- c(A1,A2,A3,A4,A5,A6,A7,A8)
+    incProgress(amount = 1/6)
+    
+    for (i in 1:8)
+    {
+      resp <- visioncounts_perwell(i+8, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
+      }
+      assign(paste("B",i, sep=""), resp)
+    }
+    rowB <- c(B1,B2,B3,B4,B5,B6,B7,B8)
+    incProgress(amount = 2/6)
+    
+    for (i in 1:8)
+    {
+      resp <- visioncounts_perwell(i+16, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
+      }
+      assign(paste("C",i, sep=""), resp)
+    }
+    rowC <- c(C1,C2,C3,C4,C5,C6,C7,C8)
+    incProgress(amount = 3/6)
+    
+    
+    for (i in 1:8)
+    {
+      resp <- visioncounts_perwell(i+24, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
+      }
+      assign(paste("D",i, sep=""), resp)
+    }
+    rowD <- c(D1,D2,D3,D4,D5,D6,D7,D8)
+    incProgress(amount = 4/6)
+    
+    
+    for (i in 1:8)
+    {
+      resp <- visioncounts_perwell(i+32, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
+      }
+      assign(paste("E",i, sep=""), resp)
+    }
+    rowE <- c(E1,E2,E3,E4,E5,E6,E7,E8)
+    incProgress(amount = 5/6)
+    
+    
+    for (i in 1:8)
+    {
+      resp <- visioncounts_perwell(i+40, rawdata, time1, time2, time3, time4, time5, threshold)
+      if (is.null(resp)){
+        resp <- "NA"
+      }
+      assign(paste("F",i, sep=""), resp)
+    }
+    rowF <- c(F1,F2,F3,F4,F5,F6,F7,F8)
+    incProgress(amount = 1)
+    
+
+    
+    wells <- c("Well 1", "Well 2", "Well 3", "Well 4", "Well 5", "Well 6", "Well 7", "Well 8")
+    df <- data.frame(wells, rowA,rowB,rowC,rowD,rowE,rowF)
+    
+    
+    
+  }
       })
       
       ## Allow downloading of CSV file of df table, this must be within the scope of the generated df
